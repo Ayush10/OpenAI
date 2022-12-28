db6a5437c772c33441e6ecdaff5d476c0a477b0d
Ayush Ojha <ayushgreat10@gmail.com>
2022-12-28 22:48:30 +0545

    first commit

---
+++ client/script.js
@@ -0,0 +1,82 @@
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat-container");
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+
+    if (element.textContent.length > 3) {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let i = 0;
+
+  let interval = setInterval(() => {
+    if (i < text.length) {
+      element.innerHTML += text.charAt(i);
+      i++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const date = Date.now();
+  const random = Math.random();
+  const hexadecimalString = random.toString(16);
+
+  return `id-${date}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAi && "ai"}">
+      <div class="chat">
+        <div class="profile">
+          <img src=${isAi ? bot : user} alt="${isAi ? "bot" : "user"}" />
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div>
+    `;
+}
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chatstripe
+  form.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  form.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (event) => {
+  //  check if the key pressed is enter and then submit the form
+  if (event.key === "Enter") {
+    handleSubmit(event);
+  }
+});
